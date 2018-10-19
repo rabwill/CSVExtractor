422e2a1a7bf9fd1d8be400b472627cd8b5eefa73
Rabia Williams <rabia.williams@thesolutioncollective.com.au>
2018-10-19 20:34:18 +1000

    initial commit

--- ERM.CSV.Extractor/Classes/TOUFileExtractor.cs
+++ ERM.CSV.Extractor/Classes/TOUFileExtractor.cs
@@ -26,6 +26,7 @@ namespace ERM.CSV.Extractor.Classes
                 List<TOUFile> currentFileValues = new List<TOUFile>();
                 currentFileValues = File.ReadAllLines(file).Skip(1).Select(v => TOUEntity.GetTOUFileEntityByCSVDataRow(v)).ToList();
                 messages = ListHelper.ListProcessor(currentFileValues, file,Constants.ColumnName.Energy,_percentage);
+
             }
 
             ConsolePrinter.PrintMessages(messages);
